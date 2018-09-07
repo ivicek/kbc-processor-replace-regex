a0d7a4f0e5f63922a87bf7dc4b17f3ace3d02bc3
ivik <ivik@igloonet.cz>
2018-09-07 17:12:19 +0200

    exec

--- replace.sh
+++ replace.sh
@@ -7,7 +7,7 @@ REPLACE_WHAT=`jq ".parameters.replace_what" -r /data/config.json`
 REPLACE_WITH=`jq ".parameters.replace_with" -r /data/config.json`
 
 
-find /data -name '*.csv'  -exec  sed -i "s/${REPLACE_WHAT}/${REPLACE_WITH}/g" {} \;
+find /data -name '*.csv' -type f | xargs -n1 sed -i "s/${REPLACE_WHAT}/${REPLACE_WITH}/g"
 
 cp -r /data/in/files /data/out/
 cp -r /data/in/tables /data/out/
